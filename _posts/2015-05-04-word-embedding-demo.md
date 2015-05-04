21713caf4e945a6fc3293d05684d9e76234fc700
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2015-05-04 03:59:01 +0300

    Add word embedding demo

---
+++ _posts/2015-05-04-word-embedding-demo.md
@@ -0,0 +1,23 @@
+---
+layout: post
+title: "Word Embedding Demo"
+modified:
+categories: 
+excerpt:
+tags: []
+image:
+  feature:
+date: 2015-05-04T03:29:16+03:00
+---
+
+<link rel="stylesheet" href="/assets/css/words.css">
+<div class="wrapper">
+  <form>
+    <input class="form-control" type="text" placeholder="Въведи израз" id="expression" autocapitalize="off" />
+  </form>
+  <button class="btn btn-success btn-lg" id="vectorize">Векторизирай!</button>
+
+  <div id="results"></div>
+</div>
+<script src="/assets/js/vendor/jquery-1.9.1.min.js"></script>
+<script src="/assets/js/words.js"></script>
---
+++ assets/js/words.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  var vectorize = function () {
+    var expr = $('#expression').val();
+    console.log(expr);
+    $.get('http://ml.yuhu.biz/vectorize', {expression: expr}, function (data) {
+      console.log(data);
+      var html = '<div>Най-близки думи:</div><ol>';
+      data.words.forEach(function (word) {
+        html += '<li>' + word + '</li>';
+      });
+      html += '</ol>';
+      $('#results').html(html);
+    });
+  };
+  $('#vectorize').click(vectorize);
+  $('#vectorize').on('touchend', vectorize);
+  $('#expression').keydown(function (event) {
+    if (event.keyCode == 13) {
+      vectorize();
+      return false;
+    }
+  })
+});
